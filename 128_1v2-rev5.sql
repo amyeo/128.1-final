59296a851d5c161bd23331928089fc670258b408
Alex <amyeo@up.edu.ph>
2019-05-15 21:16:50 +0800

    Updated database to latest revision

--- 128_1v2-rev5.sql
+++ 128_1v2-rev5.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 02, 2019 at 06:27 AM
+-- Generation Time: May 15, 2019 at 03:07 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.4
 
@@ -57,7 +57,7 @@ CREATE TABLE `applicants` (
   `civil_status` enum('S','M') COLLATE utf8mb4_bin DEFAULT NULL,
   `nationality` varchar(15) COLLATE utf8mb4_bin DEFAULT NULL,
   `SSN` varchar(15) COLLATE utf8mb4_bin DEFAULT NULL,
-  `target_position` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `target_position` int(10) UNSIGNED NOT NULL,
   `birth_certificate_id` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
   `user_password` varchar(500) COLLATE utf8mb4_bin DEFAULT NULL,
   `skills_csv` varchar(6000) COLLATE utf8mb4_bin DEFAULT NULL,
@@ -65,6 +65,14 @@ CREATE TABLE `applicants` (
   `approved` tinyint(1) DEFAULT '0',
   `do_not_show` tinyint(1) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Dumping data for table `applicants`
+--
+
+INSERT INTO `applicants` (`id`, `first_name`, `last_name`, `middle_initial`, `sex`, `current_address`, `place_of_birth`, `birthdate`, `phone_number`, `email`, `linkedin_profile`, `educational_attainment`, `civil_status`, `nationality`, `SSN`, `target_position`, `birth_certificate_id`, `user_password`, `skills_csv`, `job_history_csv`, `approved`, `do_not_show`) VALUES
+(1, 'Shiro', 'Nai', 'K', 'F', 'Block 5 Lot 11 B.F. Homes, Paranaque City, Metro Manila.', 'Quezon City', '2007-11-00', '090543216969', 'nai@gmail.com', 'linkedin.com/nainanikusorayo', 'College', 'M', 'Filipino', '7646359856', 4, '83294832474', '12345', NULL, NULL, 2, 0),
+(2, '砂糖', '中野', '無い', NULL, '２３F田中建物中央区東京', '長野', '2000-01-01', '54759843', 'ilovemaids@yahoo.com', 'linkedin.com/kimochiii', 'College', 'M', 'Chinese', '43232532', 1, '4532543533', '12345', NULL, NULL, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -88,10 +96,19 @@ CREATE TABLE `employee` (
   `civil_status` enum('S','M') COLLATE utf8mb4_bin DEFAULT NULL,
   `nationality` varchar(15) COLLATE utf8mb4_bin DEFAULT NULL,
   `SSN` varchar(15) COLLATE utf8mb4_bin DEFAULT NULL,
-  `target_position` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `job_position` varchar(50) COLLATE utf8mb4_bin NOT NULL,
   `birth_certificate_id` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
-  `employment_date` datetime DEFAULT CURRENT_TIMESTAMP
+  `employment_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `health_package` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`id`, `first_name`, `last_name`, `middle_initial`, `sex`, `current_address`, `place_of_birth`, `birthdate`, `phone_number`, `email`, `linkedin_profile`, `educational_attainment`, `civil_status`, `nationality`, `SSN`, `job_position`, `birth_certificate_id`, `employment_date`, `health_package`) VALUES
+(1, 'John', 'Solis', 'M', 'M', '16 Mariano street, KL Village, Quezon City, Manila', 'Quezon City', '2000-01-02', '53634454', 'msolis@company.com', NULL, 'Vocational', 'M', 'Filipino', '435354', 'Janitor 1', '564565645', '2019-05-08 11:05:53', 1),
+(2, 'Reno', 'Villar', 'R', 'M', 'B4 L23 AK housing village, Quezon City', 'Manila', '1997-05-01', '43453242', 'avil@company.com', 'linkedin.com/bestjanitor', 'Highschool', 'S', 'Filipino', '43252432', 'Janitor', '5436563', '2019-05-08 11:09:52', 2);
 
 -- --------------------------------------------------------
 
@@ -105,6 +122,14 @@ CREATE TABLE `employee_deps` (
   `dep_name` varchar(500) COLLATE utf8mb4_bin DEFAULT NULL,
   `dep_relation` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Dumping data for table `employee_deps`
+--
+
+INSERT INTO `employee_deps` (`id`, `employee_id`, `dep_name`, `dep_relation`) VALUES
+(1, 1, 'Maria Solis', 'mother'),
+(2, 1, 'Mariano Solis', 'brother');
 
 -- --------------------------------------------------------
 
@@ -121,6 +146,13 @@ CREATE TABLE `employee_job_history` (
   `year_ended` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
+--
+-- Dumping data for table `employee_job_history`
+--
+
+INSERT INTO `employee_job_history` (`id`, `employee_id`, `job_name`, `job_desc`, `year_started`, `year_ended`) VALUES
+(1, 1, 'Janitor', 'Basic Janitor at company XYZ', 1990, 1997);
+
 -- --------------------------------------------------------
 
 --
@@ -132,6 +164,57 @@ CREATE TABLE `employee_skills` (
   `employee_id` int(10) UNSIGNED NOT NULL,
   `skill` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Dumping data for table `employee_skills`
+--
+
+INSERT INTO `employee_skills` (`id`, `employee_id`, `skill`) VALUES
+(1, 1, 'Carpentry');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `health_packages`
+--
+
+CREATE TABLE `health_packages` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `package_name` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+  `package_desc` varchar(600) COLLATE utf8mb4_bin DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Dumping data for table `health_packages`
+--
+
+INSERT INTO `health_packages` (`id`, `package_name`, `package_desc`) VALUES
+(1, 'Package A', 'Basic SSS contributions monthly.'),
+(2, 'Package B', 'Basic SSS contributions and basic health insurance.'),
+(3, 'Package C', 'Basic SSS contributions and premium health insurance.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job_positions`
+--
+
+CREATE TABLE `job_positions` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `job_title` varchar(100) COLLATE utf8mb4_bin NOT NULL,
+  `job_description` varchar(500) COLLATE utf8mb4_bin DEFAULT NULL,
+  `is_open` tinyint(1) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Dumping data for table `job_positions`
+--
+
+INSERT INTO `job_positions` (`id`, `job_title`, `job_description`, `is_open`) VALUES
+(1, 'Janitor 1', 'Basic entry level janitor. Basic salary and starting pay. with health benefits.', 1),
+(2, 'Secretary 1', 'Entry level secretary. Basic benefits.', 1),
+(3, 'Receptionist 2', 'Receptionist with at least 1 year experience in the bob or any related job. Basic benefits.', 1),
+(4, 'Maid', 'この仕事は日本のマイドの意味です。マイド喫茶店のようにをします。「お帰りなさいませご主人様！」と言うできますから。', 1);
 
 --
 -- Indexes for dumped tables
@@ -147,13 +230,15 @@ ALTER TABLE `admin`
 -- Indexes for table `applicants`
 --
 ALTER TABLE `applicants`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `target_position` (`target_position`);
 
 --
 -- Indexes for table `employee`
 --
 ALTER TABLE `employee`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `health_package` (`health_package`);
 
 --
 -- Indexes for table `employee_deps`
@@ -177,6 +262,18 @@ ALTER TABLE `employee_skills`
   ADD KEY `employee_id` (`employee_id`);
 
 --
+-- Indexes for table `health_packages`
+--
+ALTER TABLE `health_packages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `job_positions`
+--
+ALTER TABLE `job_positions`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -190,35 +287,59 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `applicants`
 --
 ALTER TABLE `applicants`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `employee`
 --
 ALTER TABLE `employee`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `employee_deps`
 --
 ALTER TABLE `employee_deps`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `employee_job_history`
 --
 ALTER TABLE `employee_job_history`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `employee_skills`
 --
 ALTER TABLE `employee_skills`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `health_packages`
+--
+ALTER TABLE `health_packages`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `job_positions`
+--
+ALTER TABLE `job_positions`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `applicants`
+--
+ALTER TABLE `applicants`
+  ADD CONSTRAINT `applicants_ibfk_1` FOREIGN KEY (`target_position`) REFERENCES `job_positions` (`id`);
+
+--
+-- Constraints for table `employee`
+--
+ALTER TABLE `employee`
+  ADD CONSTRAINT `employee_ibfk_1` FOREIGN KEY (`health_package`) REFERENCES `health_packages` (`id`);
 
 --
 -- Constraints for table `employee_deps`
